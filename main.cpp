391e4672c47e6852e58cfa43e5c9d1d502cdbe90
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-05-27 23:03:45 -0700

    Clean up program_options includes

--- main.cpp
+++ main.cpp
@@ -15,9 +15,7 @@
 #include <iostream>
 #include <fstream>
 #include <random>
-#include "boost/program_options/options_description.hpp"
-#include "boost/program_options/parsers.hpp"
-#include "boost/program_options/variables_map.hpp"
+#include "boost/program_options.hpp"
 
 namespace po = boost::program_options;
 
